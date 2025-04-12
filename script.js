0d2d503e0d8f5ee03cf69e76ef0904854df890d6
Harsh Dhiman <harshdhiman484@gmail.com>
2025-04-12 11:56:37 +0530

    modilfy the script

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
 let data = document.getElementById("inputData");
 btn.addEventListener("click", (e) => {
   e.preventDefault();
-  if (data.value == "") {
-    alert("PlEASE PROVIDE SOME TEXT");
-  } else {
-    let elm = document.createElement("tr");
-    let elm1 = document.createElement("td");
-    elm1.innerText = data.value;
-    let elm2 = document.createElement("td");
-    elm2.innerText = "Active";
-    let elm3 = document.createElement("td");
-elm3.className = "dltndmore"
-    let elm4 = document.createElement("input");
-    elm4.setAttribute("type", "checkbox");
-    elm4.className = "checkbox";
-
-    elm4.addEventListener("change", function () {
-      if (elm4.checked) {
-        elm.style.textDecoration = "line-through";
-        elm.style.color = "red";
-        elm2.innerText = "completed";
-      } else {
-        elm.style.textDecoration = "none";
-        elm.style.color = "black";
-        elm2.innerText = "active";
-      }
-    });
-    let elm5 = document.createElement("i");
-    elm5.className = "fa-solid fa-trash";
-    elm5.style.color = "#e90707";
-    elm5.addEventListener('click',(e)=>{
-      e.preventDefault()
-      elm.remove()
-    })
-    document.getElementsByTagName("table")[0].appendChild(elm);
-    elm.appendChild(elm1);
-    elm.appendChild(elm2);
-    elm.appendChild(elm3);
-    elm3.appendChild(elm4);
-    elm3.appendChild(elm5);
+  let arr = [];
+  if (arr.includes(data.value)) {
+    return alert("no this is");
+  } 
+  else if (data.value !== "") {
+    arr.push(data.value);
+  } 
+  else {
+    return alert("please enter some value");
   }
-});
 
-let a = 0;
-let b = 0;
-console.log();
+  let elm = document.createElement("tr");
+  let elm1 = document.createElement("td");
+  elm1.innerText = data.value;
+  let elm2 = document.createElement("td");
+  elm2.innerText = "Active";
+  let elm3 = document.createElement("td");
+  elm3.className = "dltndmore";
+  let elm4 = document.createElement("input");
+  elm4.setAttribute("type", "checkbox");
+  elm4.className = "checkbox";
+
+  elm4.addEventListener("change", function () {
+    if (elm4.checked) {
+      elm.style.textDecoration = "line-through";
+      elm.style.color = "red";
+      elm2.innerText = "completed";
+    } else {
+      elm.style.textDecoration = "none";
+      elm.style.color = "black";
+      elm2.innerText = "active";
+    }
+  });
+  let elm5 = document.createElement("i");
+  elm5.className = "fa-solid fa-trash";
+  elm5.style.color = "#e90707";
+  elm5.addEventListener("click", (e) => {
+    e.preventDefault();
+    elm.remove();
+  });
+  document.getElementsByTagName("table")[0].appendChild(elm);
+  elm.appendChild(elm1);
+  elm.appendChild(elm2);
+  elm.appendChild(elm3);
+  elm3.appendChild(elm4);
+  elm3.appendChild(elm5);
+  data.value = "";
+});
